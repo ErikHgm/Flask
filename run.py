592ce04e7689b65c38f1323353655d1ac0f60f0d
Erik <erikhgm@gmail.com>
2022-08-16 11:52:54 +0000

    Update contact view and template

--- run.py
+++ run.py
@@ -1,12 +1,14 @@
 import os
 import json
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
 
 @app.route("/about")
 def about():
@@ -26,8 +28,11 @@ def about_member(member_name):
                 member = obj
     return render_template("member.html", member=member)
 
-@app.route("/contact")
+
+@app.route("/contact", methods=["GET", "POST"])
 def contact():
+    if request.method == "POST":
+        print(request.form)
     return render_template("contact.html", page_title="Contact")
 
 
